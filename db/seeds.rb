67c9b6f9648dad93ecd3c14e9033adb3a44fe456
Cole Rau <colevrau@gmail.com>
2020-10-16 23:44:23 -0700

    set up models and associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u1 = User.create(username: "cole")
+u2 = User.create(username: "haven")
+
+p1 = Photo.create(nasa_photo_id: 35423, user_id: 1)
+p2 = Photo.create(nasa_photo_id: 9, user_id: 1)
+p3 = Photo.create(nasa_photo_id: 980, user_id: 2)
+p4 = Photo.create(nasa_photo_id: 321111, user_id: 2)
+
