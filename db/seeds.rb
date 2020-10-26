fff0a87b0c98b9a610a9415689d62153cd7b80bd
Cole Rau <colevrau@gmail.com>
2020-10-26 01:34:36 -0700

    got rid of photo seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@
 u1 = User.create(username: "cole")
 u2 = User.create(username: "haven")
 
-p1 = Photo.create(nasa_photo_id: 35423, user_id: 1)
-p2 = Photo.create(nasa_photo_id: 9, user_id: 1)
-p3 = Photo.create(nasa_photo_id: 980, user_id: 2)
-p4 = Photo.create(nasa_photo_id: 321111, user_id: 2)
+# p1 = Photo.create(nasa_photo_id: 35423, user_id: 1)
+# p2 = Photo.create(nasa_photo_id: 9, user_id: 1)
+# p3 = Photo.create(nasa_photo_id: 980, user_id: 2)
+# p4 = Photo.create(nasa_photo_id: 321111, user_id: 2)
 
