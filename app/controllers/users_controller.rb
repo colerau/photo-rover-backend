82d29a5b51191763e3d20a15faad3606d478042f
Cole Rau <colevrau@gmail.com>
2020-10-26 15:43:03 -0700

    rendering user and users photos in sessions#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   def create
       user = User.new(user_params)
       if user.save
-          session[:user_id] = user.id 
+          session[:user_id] = user.id
           render json: user
       else
           render json: user.errors.full_messages
