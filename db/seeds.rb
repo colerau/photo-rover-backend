33a1ccce4a4a8f461704d0b264f711b08ce02a5e
Cole Rau <colevrau@gmail.com>
2020-10-26 21:56:28 -0700

    on way to getting to user saved photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ u1 = User.create(username: "cole")
 u2 = User.create(username: "haven")
 
 # p1 = Photo.create(nasa_photo_id: 35423, user_id: 1)
-p2 = Photo.create(user_id: 1)
-p3 = Photo.create(user_id: 2)
+# p2 = Photo.create(user_id: 1)
+# p3 = Photo.create(user_id: 2)
 # p4 = Photo.create(nasa_photo_id: 321111, user_id: 2)
 
